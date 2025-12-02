33610de87aaa9ea4d40fe2041ba4ec11bd02fd8f
ibadvahidov505 <ibadvahidov505@gmail.com>
2025-12-02 15:42:03 +0000

    ibad

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
