c681a6ae76f57d314493792d9da74d69e3b8dbe7
ibadvahidov505 <ibadvahidov505@gmail.com>
2025-12-10 11:16:40 +0000

    elmin

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,24 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints numbers in a nested loop example
+ * print_most_numbers - prints numbers 0 to 14, ten times
  *
  * Return: void
  */
 void print_most_numbers(void)
 {
-	int a, b;
+	int a, b, c;
 
-	for (a = 0; a < 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b < 15; b++)
 		{
-			_putchar(b + '0');
+			if (b > 9)
+				_putchar('1');
+
+			c = b % 10;
+			_putchar(c + '0');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
