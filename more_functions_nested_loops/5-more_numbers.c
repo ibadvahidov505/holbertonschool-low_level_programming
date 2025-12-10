b581710c9fab947bcc666122e502bba4478d0928
ibadvahidov505 <ibadvahidov505@gmail.com>
2025-12-10 11:22:34 +0000

    elmin

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers 0 to 14, ten times
+ * more_numbers - prints numbers 0 to 14 ten times
  *
  * Return: void
  */
-void print_most_numbers(void)
+void more_numbers(void)
 {
 	int a, b, c;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 15; b++)
 		{
