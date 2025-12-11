78ef886bab0fc322b953fafaa98cebb0a68870aa
ibadvahidov505 <ibadvahidov505@gmail.com>
2025-12-12 00:36:07 +0400

    elmin

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints all single-digit numbers of base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
