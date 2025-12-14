ec080670e8cb8042d14d22c08857701ffcdd9988
ibadvahidov505 <ibadvahidov505@gmail.com>
2025-12-14 19:11:11 +0400

    elmin

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: the string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+			_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
+
