7944e0cba43310f1d0d4beccadd510c95a9c47ab
ibadvahidov505 <ibadvahidov505@gmail.com>
2025-12-21 23:06:23 +0400

    Add files via upload

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if success, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int result;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
